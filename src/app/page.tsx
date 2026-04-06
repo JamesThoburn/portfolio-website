4162e38b69973c244eba41a6086c32522a597640
James Thoburn <james_thoburn@outlook.com>
2026-04-06 09:17:30 +0100

    Improved padding for mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="max-w-2xl mx-auto">
+    <div className="max-w-2xl mx-auto p-6">
       <Hero />
     </div>
   );
--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -15,7 +15,7 @@ const socialLinks = [
 
 export default function Hero() {
   return (
-    <section className="mt-20 flex flex-col p-2 gap-4">
+    <section className="mt-10 flex flex-col gap-4">
         <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-bold">Hi, I'm James Thoburn.</h1>
             <h2 className="text-gray-600">Mathematics student at Durham University.</h2>
@@ -35,13 +35,13 @@ export default function Hero() {
             </ul>
         </div>
         <p>
-            I'm a 2nd year Mathematics student at Durham University with a deep interest in both software engineering and data science. I have technical skills in both of these fields, and I am currently seeking experience opportunities in either area.
+            I'm a 2nd year Mathematics student at Durham University with a deep interest in software engineering and data science. I have technical skills in both of these fields and I am currently seeking experience in either area.
         </p>
         <p>
-            So far in my studies, I have developed a strong foundation in mathematical concepts, with my focus for 3rd and 4th year being on machine learning, data science and statistics.
+            So far in my studies, I have developed a strong foundation in mathematical concepts, with my focus for 3rd and 4th year being machine learning, data science and statistics.
         </p>
         <p>
-            I have also developed my web development skills through personal study, and I am currently focusing my efforts on learning some backend technologies, of which I will use to add to my currently frontend-focused set of technical skills.
+            I have also developed my web development skills through personal study, and I am currently focusing my efforts on learning backend technologies, of which will be added to my currently frontend-focused set of technical skills.
         </p>
     </section>
   )
