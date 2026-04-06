aea0026a14ceb024b867304d12544293f38467b9
James Thoburn <james_thoburn@outlook.com>
2026-04-06 09:19:34 +0100

    Improved padding for mobile devices

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,7 +2,7 @@ import ContactForm from "@/components/Contact/ContactForm";
 
 export default function Contact() {
   return (
-    <div className='max-w-2xl mx-auto mt-10'>
+    <div className='max-w-2xl mx-auto mt-10 p-6'>
         <ContactForm />
     </div>
   )
